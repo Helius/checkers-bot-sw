97eef7cc3db04c83928c5ba5f64556cdf4cf8e3a
Eugene Samoylov <e.samoylov@2gis.ru>
2019-11-05 21:07:36 +0700

    start writing classes for mecanic part

--- app/main.cpp
+++ app/main.cpp
@@ -10,40 +10,99 @@
 #include <uart.h>
 #include <misc.h>
 
-void togglePin(void)
-{
-	// toggle pin
-	PORTB ^= (1 << PIN5);
-	// wait for 250ms
-	_delay_ms(250);
-}
+// Types
 
+class StepMotor {
+	public:
+		StepMotor(OutPin dirPin) 
+		: dir(dirPin)
+		{
+		}
 
+		bool busy() {
+			return stepCount;
+		}
+
+		void moveAngle(uint8_t angle, bool direction) {
+			if(!stepCount) {
+				moveSteps(stepsPerAngle(angle), direction);
+			}
+		}
+
+		void onStepHandler()
+		{
+			if(stepCount--) {
+				// handle timer speed
+			} else {
+				// done, stop timer
+			}
+		}
+		
+	private:
+		// we have 10 gear ration and 400 steps per rotation;
+		// 4000 steps = 360 grad
+		//   ?  steps = x grad
+		uint16_t stepsPerAngle(uint8_t angle) {
+			return (100*angle)/9;
+		}
+		
+		void moveSteps(uint16_t steps, bool direction) {
+			if(direction) {
+				dir.set();
+			} else {
+				dir.clear();
+			}
+			stepCount = steps;
+			// start timer
+		}
+
+	private:
+		OutPin dir;
+		uint16_t stepCount = 0;
+};
+
+class MecanicalArm {
+	public:
+		MecanicalArm() = default;
+	private:
+		StepMotor m1;
+		StepMotor m2;
+};
+
+// Objects
+OutPin led(&DDRB, &PORTB, PIN5);   // on board led
+OutPin m1dir(&DDRB, &PORTB, PIN0); // PB0 is direction for motor 1
+OutPin m2dir(&DDRD, &PORTD, PIN7); // PD7 is direction for motor 2
+
+StepMotor m1(m1dir);
+StepMotor m2(m2dir);
+
+// ISR Handlers
 void uart_rx_handler(unsigned char ch)
 {
-	togglePin();
+}
+
+// Initialisation
+void timer1init()
+{
+}
+
+void externalInterruptInit()
+{
+
 }
 
 int main(void)
 {
-	
 	uart_init();
 	set_receive_interrupt_handler(&uart_rx_handler);
-	msg("hello world, how are you?\n\r");
-	
-	DDRB |= (1 << PIN5);
-
-	togglePin();
-
-	// delay for enabling bluetooth board power
-	// allow to program board via serial port
-	_delay_ms(3000);
+	msg("\n\r\n\rCheckersBot v1.0\n\r\n\r");
 
 	sei();
-
 	while(1)
 	{
-		togglePin();
+		_delay_ms(200);
+		led.toggle();
 	}
 }
 
