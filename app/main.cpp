c014ef59591a9db7a7b8fa123eeab398d546526b
Eugene Samoylov <e.samoylov@2gis.ru>
2019-11-06 00:13:31 +0700

    use timers and ISR and connect to motor

--- app/main.cpp
+++ app/main.cpp
@@ -14,9 +14,10 @@
 
 class StepMotor {
 	public:
-		StepMotor(OutPin & dirPin, InPin & zeroPin) 
+		StepMotor(OutPin & dirPin, InPin & zeroPin, volatile uint8_t * reg) 
 		: dir(dirPin)
 		, zero(zeroPin)
+		, value(reg)
 		{
 		}
 
@@ -43,8 +44,10 @@ class StepMotor {
 		void onStepHandler() {
 			if(stepCount--) {
 				// handle timer speed
+				value++;
 			} else {
 				// done, stop timer
+				value = 0;
 			}
 		}
 		
@@ -71,6 +74,7 @@ class StepMotor {
 		InPin & zero;
 		uint8_t lastAngle = 0;
 		uint16_t stepCount = 0;
+		volatile uint8_t * value;
 };
 
 class MecanicalArm {
@@ -103,8 +107,8 @@ OutPin m1dir(&DDRD, &PORTD, PIN7); // PD7 is direction for motor 2
 InPin zero0pin(&DDRD, &PORTD, &PIND, PIN2); // ext0 pin
 InPin zero1pin(&DDRD, &PORTD, &PIND, PIN3); // ext1 pin
 
-StepMotor m0(m0dir, zero0pin);
-StepMotor m1(m1dir, zero1pin);
+StepMotor m0(m0dir, zero0pin, &OCR0A);
+StepMotor m1(m1dir, zero1pin, &OCR1AL);
 MecanicalArm arm(m0, m1);
 
 // ISR Handlers
@@ -128,18 +132,44 @@ ISR(INT1_vect)
 	msg("zero 1\n\r");
 }
 
+OutPin tst(&DDRB, &PORTB, PIN4);
+
+ISR(TIMER1_COMPA_vect)
+{
+	static bool toggle = 0;
+	if(toggle) {
+		m0.onStepHandler();
+	}
+	toggle = !toggle;
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+	static bool toggle = 0;
+	if(toggle) {
+		m1.onStepHandler();
+	}
+	toggle = !toggle;
+}
+
 int main(void)
 {
 	// setup external interrupt
 	EICRA = (1 << ISC01) | (1 << ISC11);
 	EIMSK = (1 << INT0) | (1 << INT1);
 	
-	// timer 1 init for motors pulse handling
-	
-	TCCR0A = _BV(WGM01) | _BV(WGM00) | _BV(COM0A1) | _BV(COM0B1); // fast pwm, non-invert
-	TCCR0B = _BV(CS01) | _BV(CS00);  // prescaller is 32
+	// timer 0 and 1 init for motors pulse handling
+	DDRD |= _BV(PD6);
 	OCR0A  = 0;
-	OCR0B  = 0;
+	TCCR0A = _BV(WGM01) | _BV(COM0A0);
+	TCCR0B = _BV(CS02);
+	TIMSK0 = _BV(OCIE0A);
+	
+	DDRB |= _BV(PB1);
+	OCR1A  = 0;
+	TCCR1A = _BV(COM1A0);
+	TCCR1B = _BV(WGM12) | _BV(CS12);
+	TIMSK1 = _BV(OCIE1A);
 	
 
 	uart_init(0);
